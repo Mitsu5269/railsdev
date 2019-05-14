7b2fa6a687aee4a66364221ec2fd68f81ea78947
Mitsu5269 <4617069@ed.tus.ac.jp>
2019-05-14 09:00:51 +0900

    make CRU

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,7 +4,21 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
-    def index
-        @articles = Article.find(params[:id])
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to articles_path
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:name, :description)
     end
 end
