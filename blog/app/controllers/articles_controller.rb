dfd81e693f1d4b7eab3279eda7fa8a27e51efde3
Mitsu5269 <4617069@ed.tus.ac.jp>
2019-05-15 16:18:45 +0900

    making update

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,24 +1,36 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
+        @article = Article.new
     end
 
     def create
-        @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_path
+        article = Article.new(article_params)
+        article.save!
+        redirect_to articles_url, notice: "新しい記事を登録しました。"
+    end
+
+    def edit
+    end
+
+    def update
+        @article.update!(article_params)
+        redirect_to articles_url, notice: "記事を更新しました。"
     end
 
     private
     def article_params
         params.require(:article).permit(:name, :description)
     end
+
+    def set_article
+        @task = article.find(params[:id])
 end
